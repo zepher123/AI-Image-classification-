999fa81beed298ba8c23ce079df6fb318369db45
Utsav Paudel <utsavpaudel29@gmail.com>
2023-02-10 14:58:45 +0545

    image prediction print

--- image_recog.py
+++ image_recog.py
@@ -1,7 +1,6 @@
 from imageai.Classification import ImageClassification
 import os
 
-import os
 execution_path = os.getcwd()
 
 prediction = ImageClassification()
@@ -9,5 +8,7 @@ prediction.setModelTypeAsResNet50()
 prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth"))
 prediction.loadModel()
 
-
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "image1.jpg"), result_count=10)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
 
